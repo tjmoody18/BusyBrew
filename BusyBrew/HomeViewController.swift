51377ea1186a5cc9552f0d58646c22dde502b8a1
Thomas Moody <tjmoody18@gmail.com>
2025-03-09 17:02:31 -0500

    Configure MapKit, and HomeViewController with base MapView and search

---
+++ BusyBrew/HomeViewController.swift
@@ -0,0 +1,60 @@
+//
+//  HomeViewController.swift
+//  BusyBrew
+//
+//  Created by Thomas Moody on 3/9/25.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class HomeViewController: UIViewController, CLLocationManagerDelegate{
+
+    lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+//        mapView.showUserLocation = true
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    lazy var searchTextField: UITextField = {
+        let searchTextField = UITextField()
+        searchTextField.layer.cornerRadius = 10
+        searchTextField.clipsToBounds = true
+        searchTextField.backgroundColor = UIColor.white
+        searchTextField.placeholder = "Search"
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        searchTextField.leftViewMode = .always
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
+    }()
+    
+    let locationManager = CLLocationManager() // CoreLocation init location Manager
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupUI() {
+        view.addSubview(searchTextField)
+        view.addSubview(mapView)
+        view.bringSubviewToFront(searchTextField)
+        
+        // search text field constraints
+        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width / 1.2).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        searchTextField.returnKeyType = .go
+        
+        // map view contraints
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+}
