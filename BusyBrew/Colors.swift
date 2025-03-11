a0ae2f54f267c767dd63875e8713f9118fada821
Donald <donaldfromthailand73@gmail.com>
2025-03-11 18:36:14 -0500

    Added in colors

---
+++ BusyBrew/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  BusyBrew
+//
+//  Created by Donald Thai on 3/11/25.
+//
+
+import UIKit
+
+// #526843
+let background1 = UIColor(red: 82/255.0, green: 104/255.0, blue: 67/255.0, alpha: 1.0)
+
+// #7A9B64
+let background1Light = UIColor(red: 122/255.0, green: 155/255.0, blue: 100/255.0, alpha: 1.0)
+
+// #C29953
+let background2 = UIColor(red: 194/255.0, green: 153/255.0, blue: 83/255.0, alpha: 1.0)
