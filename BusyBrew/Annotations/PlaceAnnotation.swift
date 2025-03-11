dd2bc460cfdb04ad9935fa38f6563a969c8c3782
Thomas Moody <tjmoody18@gmail.com>
2025-03-10 19:09:33 -0500

    Add properties to PlaceAnnotation

--- BusyBrew/Annotations/PlaceAnnotation.swift
+++ BusyBrew/Annotations/PlaceAnnotation.swift
@@ -8,14 +8,31 @@
 import Foundation
 import MapKit
 
-class PlaceAnnotation: NSObject, MKAnnotation {
-    var location: CLLocation
-    var coordinate: CLLocationCoordinate2D
-    var name: String?
+class PlaceAnnotation: MKPointAnnotation {
+    
+    let mapItem: MKMapItem
+    let id = UUID()
+    var isSelected: Bool = false
     
     init(mapItem: MKMapItem) {
-        self.name = mapItem.name
-        self.location = mapItem.placemark.location!
+        self.mapItem = mapItem
+        super.init()
         self.coordinate = mapItem.placemark.coordinate
+    }
+    
+    var name: String {
+        mapItem.name ?? ""
+    }
+    
+    var phone: String? {
+        mapItem.phoneNumber ?? ""
+    }
+    
+    var address: String {
+        "\(mapItem.placemark.subThoroughfare ?? "") \(mapItem.placemark.thoroughfare ?? "") \(mapItem.placemark.locality ?? "") \(mapItem.placemark.countryCode ?? "")"
+    }
+    
+    var location: CLLocation {
+        mapItem.placemark.location ?? CLLocation.default
     }
 }
---
+++ BusyBrew/Extensions/CLLocation+Extentions.swift
@@ -0,0 +1,16 @@
+//
+//  CLLocation+Extentions.swift
+//  BusyBrew
+//
+//  Created by Thomas Moody on 3/10/25.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLLocation{
+    
+    static var `default`: CLLocation {
+        CLLocation(latitude: 32.6514, longitude: -161.4333)
+    }
+}
