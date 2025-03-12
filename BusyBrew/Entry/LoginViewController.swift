3196add78a5f2bd407b9e41cae1abcd3e7d4c148
Donald <donaldfromthailand73@gmail.com>
2025-03-11 20:11:23 -0500

    Added in Login and Register pages

--- BusyBrew/Entry/LoginViewController.swift
+++ BusyBrew/Entry/LoginViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    let backButton = UIButton(type: .system)
+    let pageTitle = UILabel()
+    let userTextField = UITextField()
+    let passwordTextField = UITextField()
+    let loginButton = UIButton(type: .system)
     let toHomeSegueIdentifier = "LoginToHomeSegue"
     let toRegisterSegueIdentifer = "RegisterSegue"
 
@@ -19,17 +24,11 @@ class LoginViewController: UIViewController {
     
     func setupUI() {
         view.backgroundColor = background1
-        let button = UIButton(type: .system)
-        button.setTitle("temp to home", for: .normal)
-        button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
-        button.addTarget(self, action: #selector(loginButtonClicked), for: .touchUpInside)
-        self.view.addSubview(button)
-        
-        let button2 = UIButton(type: .system)
-        button2.setTitle("temp to register", for: .normal)
-        button2.frame = CGRect(x: 100, y: 300, width: 200, height: 50)
-        button2.addTarget(self, action: #selector(registerButtonClicked), for: .touchUpInside)
-        self.view.addSubview(button2)
+        createBackButton()
+        createTitle()
+        createUserField()
+        createPasswordField()
+        createLoginButton()
     }
     
     @objc func loginButtonClicked() {
@@ -40,15 +39,163 @@ class LoginViewController: UIViewController {
         performSegue(withIdentifier: toRegisterSegueIdentifer, sender: self)
     }
     
+    func createTitle() {
+        pageTitle.text = "Login"
+        pageTitle.textAlignment = .left
+        pageTitle.textColor = .white
+        let attributes: [NSAttributedString.Key: Any] = [
+            .kern: -2.0  // Negative value to reduce the letter spacing
+        ]
+        pageTitle.attributedText = NSAttributedString(string: pageTitle.text ?? "", attributes: attributes)
+        self.view.addSubview(pageTitle)
+        pageTitle.translatesAutoresizingMaskIntoConstraints = false
+        pageTitle.font = UIFont.systemFont(ofSize: 80, weight: .semibold)
+        NSLayoutConstraint.activate([
+            pageTitle.topAnchor.constraint(equalTo: backButton.bottomAnchor, constant: 15),
+            pageTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40)
+        ])
+        
+        let subtitle = UILabel()
+        let registerButton = UIButton(type: .system)
+        subtitle.text = "Don't have an account?"
+        subtitle.textColor = .white
+        subtitle.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        subtitle.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(subtitle)
+        
+        registerButton.setTitle("Register now", for: .normal)
+        registerButton.setTitleColor(.white, for: .normal)
+        registerButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        let buttonAttributes: [NSAttributedString.Key: Any] = [
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        registerButton.titleLabel?.attributedText = NSAttributedString(string: registerButton.titleLabel?.text ?? "", attributes: buttonAttributes)
+        registerButton.translatesAutoresizingMaskIntoConstraints = false
+        registerButton.addTarget(self, action: #selector(registerButtonClicked), for: .touchUpInside)
+        self.view.addSubview(registerButton)
+        
+        NSLayoutConstraint.activate([
+            subtitle.topAnchor.constraint(equalTo: pageTitle.bottomAnchor, constant: 15),
+            subtitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            registerButton.centerYAnchor.constraint(equalTo: subtitle.centerYAnchor),
+            registerButton.leadingAnchor.constraint(equalTo: subtitle.trailingAnchor, constant: 7)
+        ])
+    }
+    
+    func createUserField() {
+        let userLabel = UILabel()
+        userLabel.text = "Email"
+        userLabel.textColor = .white
+        userLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        userLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(userLabel)
+        
+        let placeholderAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white  // Set the placeholder color to white
+        ]
+        userTextField.attributedPlaceholder = NSAttributedString(string: "Enter email", attributes: placeholderAttributes)
+                
+        userTextField.backgroundColor = background1Light
+        userTextField.translatesAutoresizingMaskIntoConstraints = false
+        userTextField.layer.cornerRadius = 10
+        userTextField.textColor = .white
+        userTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 0))
+        userTextField.leftViewMode = .always
+        self.view.addSubview(userTextField)
+        
+        NSLayoutConstraint.activate([
+            userLabel.topAnchor.constraint(equalTo: pageTitle.bottomAnchor, constant: 60),
+            userLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            userTextField.topAnchor.constraint(equalTo: userLabel.bottomAnchor, constant: 5),
+            userTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            userTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            userTextField.heightAnchor.constraint(equalToConstant: 45)
+        ])
+    }
+    
+    func createPasswordField() {
+        let passwordLabel = UILabel()
+        passwordLabel.text = "Password"
+        passwordLabel.textColor = .white
+        passwordLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        passwordLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(passwordLabel)
+        
+        let placeholderAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white  // Set the placeholder color to white
+        ]
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Enter password", attributes: placeholderAttributes)
+                
+        passwordTextField.backgroundColor = background1Light
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.layer.cornerRadius = 10
+        passwordTextField.textColor = .white
+        passwordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 0))
+        passwordTextField.leftViewMode = .always
+        self.view.addSubview(passwordTextField)
+        
+        NSLayoutConstraint.activate([
+            passwordLabel.topAnchor.constraint(equalTo: userTextField.bottomAnchor, constant: 20),
+            passwordLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            passwordTextField.topAnchor.constraint(equalTo: passwordLabel.bottomAnchor, constant: 5),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 45)
+        ])
+    }
+    
+    func createLoginButton() {
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.backgroundColor = background2
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.contentHorizontalAlignment = .center
+        loginButton.contentVerticalAlignment = .center
+        loginButton.layer.cornerRadius = 10
+        loginButton.addTarget(self, action: #selector(loginButtonClicked), for: .touchUpInside)
+        self.view.addSubview(loginButton)
+        
+        NSLayoutConstraint.activate([
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            loginButton.heightAnchor.constraint(equalToConstant: 45),
+            loginButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150)
+        ])
+    }
+    
+    func createBackButton() {
+        backButton.setTitle("Back", for: .normal)
+        backButton.setTitleColor(.white, for: .normal)
+        backButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        let attributes: [NSAttributedString.Key: Any] = [
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        backButton.titleLabel?.attributedText = NSAttributedString(string: backButton.titleLabel?.text ?? "", attributes: attributes)
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        self.view.addSubview(backButton)
+        NSLayoutConstraint.activate([
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+        ])
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    @objc func backButtonTapped() {
+        self.dismiss(animated: true)
+    }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
-
 }
