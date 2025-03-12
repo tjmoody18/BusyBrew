01d01f0f84545e058bb5380299f7538ce7624162
Thomas Moody <tjmoody18@gmail.com>
2025-03-12 14:48:38 -0500

    Add back buttons to settings and favorites screens

--- BusyBrew/FavoritesViewController.swift
+++ BusyBrew/FavoritesViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class FavoritesViewController: UIViewController {
 
+    let backButton = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        view.backgroundColor = background1
+        createBackButton()
     }
-    */
-
+    
+    func createBackButton() {
+        backButton.setTitle("Back", for: .normal)
+        backButton.setTitleColor(.white, for: .normal)
+        backButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        let attributes: [NSAttributedString.Key: Any] = [
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        backButton.titleLabel?.attributedText = NSAttributedString(string: backButton.titleLabel?.text ?? "", attributes: attributes)
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        self.view.addSubview(backButton)
+        NSLayoutConstraint.activate([
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+        ])
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    @objc func backButtonTapped() {
+        self.dismiss(animated: true)
+    }
 }
--- BusyBrew/SettingsViewController.swift
+++ BusyBrew/SettingsViewController.swift
@@ -9,10 +9,38 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    let backButton = UIButton(type: .system)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    func setupUI() {
+        view.backgroundColor = background1
+        createBackButton()
+    }
+    
+    func createBackButton() {
+        backButton.setTitle("Back", for: .normal)
+        backButton.setTitleColor(.white, for: .normal)
+        backButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        let attributes: [NSAttributedString.Key: Any] = [
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        backButton.titleLabel?.attributedText = NSAttributedString(string: backButton.titleLabel?.text ?? "", attributes: attributes)
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        self.view.addSubview(backButton)
+        NSLayoutConstraint.activate([
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+        ])
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    @objc func backButtonTapped() {
+        self.dismiss(animated: true)
     }
     
 
