5debf121702afa95858b5a959b53036b58cbf3e7
Thomas Moody <tjmoody18@gmail.com>
2025-04-09 15:10:45 -0500

    Add Review model class definition

--- BusyBrew/Features/Reviews/Review.swift
+++ BusyBrew/Features/Reviews/Review.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+//
+//  User.swift
+//  BusyBrew
+//
+//  Created by Thomas Moody on 4/8/25.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Review: Codable, Identifiable {
+    @DocumentID var id: String?
+    
+    var uid: String // uid of user who created review
+    var date: String
+    var wifi: Int
+    var cleanliness: Int
+    var outlets: Int
+        
+    init(uid: String, date: String, wifi: Int, cleanliness: Int, outlets: Int) {
+        self.uid = uid
+        self.date = date
+        self.wifi = wifi
+        self.cleanliness = cleanliness
+        self.outlets = outlets
+    }
+}
+
+extension Review {
+    // empty cafe on creation
+    static func empty(uid: String) -> Review {
+        return
+            Review(
+                 uid: uid,
+                 date: "",
+                 wifi: 0,
+                 cleanliness: 0,
+                 outlets: 0
+            )
+    }
+}
+
