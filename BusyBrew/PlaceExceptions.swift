af92c4fd06b366ff9bbee6b6f62f7e114c8863f9
Thomas Moody <tjmoody18@gmail.com>
2025-04-09 00:56:06 -0500

    Reorganize file layout

---
+++ BusyBrew/PlaceExceptions.swift
@@ -0,0 +1,23 @@
+//
+//  PlaceExceptions.swift
+//  BusyBrew
+//
+//  Created by Thomas Moody on 4/7/25.
+//
+
+import Foundation
+
+struct PlaceExceptions {
+    static let includeException: [String] = [
+        "Starbucks",
+        "Dunkin Donuts",
+        
+    ]
+    
+    static let excludeException: [String] = [
+        "Club",
+        "Bar",
+        "Restaurant",
+        "McDonalds"
+    ]
+}
